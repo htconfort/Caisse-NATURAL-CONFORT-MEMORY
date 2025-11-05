571323d8db11b8acdfe1a136a2ce3a86544f29ff
htconfort <ladresse@email.utilise.sur.github>
2025-11-05 14:58:12 +0100

    Ajout informations entreprise Natural Confort Memory (NCM)

--- src/config/company.ts
+++ src/config/company.ts
@@ -1,3 +1,14 @@
+// Informations entreprise Natural Confort Memory
+export const COMPANY_INFO = {
+  name: 'NATURAL CONFORT MEMORY',
+  shortName: 'NCM',
+  address: 'CARRER DE PAU CASALS, 19',
+  postalCode: '17486',
+  city: 'CASTELLO D\'EMPURIES',
+  country: 'Espagne',
+  fullAddress: 'CARRER DE PAU CASALS, 19 - 17486 CASTELLO D\'EMPURIES',
+};
+
 export const COMPANY_BANK = {
   iban: 'FR76 1660 7000 1708 1216 3980 964',
   bic: 'CCBPFRPPPPG',
